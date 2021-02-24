b9061bde47df310904c640b1509ed6e96adfb077
globlo <49570577+globlo@users.noreply.github.com>
2021-02-23 21:38:59 -0600

    add fuelquote.html

--- project.py
+++ project.py
@@ -2,6 +2,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Tina Suzuki',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+        'requested_gallons': '30',
+        'address': '2244 Bake Ave, Houston TX'
+    },
+    {
+        'author': 'Joe Sasaki',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'requested_gallons': '50',
+        'address': '2244 Bake Ave, Houston TX'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -10,6 +29,12 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route('/fuelquote')
+def fuelquote():
+    return render_template('fuelquote.html', title='Fuel Quote', post=posts[1])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
