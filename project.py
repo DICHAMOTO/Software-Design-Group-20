746b4b030804da537631969a1c3c0189653fb7b8
SongwenXue <owensit98@gmail.com>
2021-02-23 21:12:23 -0600

    Created Sample History Page

--- project.py
+++ project.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
+
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
+
+
+@app.route("/history")
+def display_history():
+    return render_template('history.html', title='History')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
