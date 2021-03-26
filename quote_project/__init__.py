b0ac7eb527d79f37b9c35b28e88fac847fd79f96
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2021-03-26 18:17:30 -0500

    added hashed password implementation

--- quote_project/__init__.py
+++ quote_project/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '788270a3a29cf81029ca3a09528ff90a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from quote_project import routes
